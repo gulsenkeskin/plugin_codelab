d06fea8bdfbc62abfe227949921e95a484f3478d
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-07-25 01:21:54 +0300

    Update main.dart

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,23 +1,26 @@
-import 'package:flutter/material.dart';
 import 'dart:async';
+import 'package:flutter/material.dart';
 
 import 'package:flutter/services.dart';
 import 'package:plugin_codelab/plugin_codelab.dart';
 
-void main() {
-  runApp(const MyApp());
+enum _KeyType { black, white }
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.landscapeRight])
+      .then((_) {
+    runApp(MyApp());
+  });
 }
 
 class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
   String _platformVersion = 'Unknown';
-  final _pluginCodelabPlugin = PluginCodelab();
 
   @override
   void initState() {
@@ -25,15 +28,14 @@ class _MyAppState extends State<MyApp> {
     initPlatformState();
   }
 
-  // Platform mesajları eşzamansızdır, bu yüzden async bir method'la başlatıyoruz.
-
+  // Platform messages are asynchronous, so we initialize in an async method.
   Future<void> initPlatformState() async {
     String platformVersion;
-// Platform mesajları başarısız olabilir, bu yüzden bir try/catch PlatformException kullanıyoruz.
-// Ayrıca potansiyel olarak null döndüren mesajı da işliyoruz.
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    // We also handle the message potentially returning null.
     try {
       platformVersion =
-          await _pluginCodelabPlugin.getPlatformVersion() ?? 'Unknown platform version';
+          await PluginCodelab.platformVersion ?? 'Unknown platform version';
     } on PlatformException {
       platformVersion = 'Failed to get platform version.';
     }
@@ -45,15 +47,64 @@ class _MyAppState extends State<MyApp> {
     });
   }
 
+  void _onKeyDown(int key) {
+    print('key down:$key');
+    PluginCodelab.onKeyDown(key).then((value) => print(value));
+  }
+
+  void _onKeyUp(int key) {
+    print('key up:$key');
+    PluginCodelab.onKeyUp(key).then((value) => print(value));
+  }
+
+  Widget _makeKey({required _KeyType keyType, required int key}) {
+    return AnimatedContainer(
+      height: 200,
+      width: 44,
+      duration: const Duration(seconds: 2),
+      curve: Curves.easeIn,
+      child: Material(
+        color: keyType == _KeyType.white
+            ? Colors.white
+            : const Color.fromARGB(255, 60, 60, 80),
+        child: InkWell(
+          onTap: () => _onKeyUp(key),
+          onTapDown: (details) => _onKeyDown(key),
+          onTapCancel: () => _onKeyUp(key),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Plugin example app'),
-        ),
+        backgroundColor: const Color.fromARGB(255, 250, 30, 0),
         body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Text('Running on: $_platformVersion\n'),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  _makeKey(keyType: _KeyType.white, key: 60),
+                  _makeKey(keyType: _KeyType.black, key: 61),
+                  _makeKey(keyType: _KeyType.white, key: 62),
+                  _makeKey(keyType: _KeyType.black, key: 63),
+                  _makeKey(keyType: _KeyType.white, key: 64),
+                  _makeKey(keyType: _KeyType.white, key: 65),
+                  _makeKey(keyType: _KeyType.black, key: 66),
+                  _makeKey(keyType: _KeyType.white, key: 67),
+                  _makeKey(keyType: _KeyType.black, key: 68),
+                  _makeKey(keyType: _KeyType.white, key: 69),
+                  _makeKey(keyType: _KeyType.black, key: 70),
+                  _makeKey(keyType: _KeyType.white, key: 71),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
